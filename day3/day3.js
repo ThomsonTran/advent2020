d54fb5ea1f4d776adc56dfa4422cc82f64c95fad
Thomson Tran <thomson.tran@gmail.com>
2020-12-05 12:44:41 -0500

    day 3

--- day3/day3.js
+++ day3/day3.js
@@ -1,24 +1,50 @@
 const fs = require("fs");
 const readline = require("readline");
 
-const readInterface = readline.createInterface({
-  input: fs.createReadStream("input.txt"),
-  output: process.stdout,
-  console: false,
+let input = fs.readFileSync("input.txt");
+input = input
+  .toString()
+  .split("\n")
+  .map((row) => row.split(""));
+
+input.forEach((row) => {
+  row.pop();
 });
 
-let result = 0;
-let step = 0;
-readInterface.on("line", function (line) {
-  let field = line.split("");
-  if (step > field.length - 1) {
-    step -= field.length;
+function getTrees(right, down) {
+  let total = 0;
+  let current = 0;
+  let width = input[0].length;
+
+  for (let i = 0; i < input.length; i += down) {
+    if (input[i][current] == "#") {
+      total++;
+    }
+    current += right;
+
+    if (current >= width) {
+      current -= width;
+    }
   }
 
-  if (field[step] == "#") {
-    result++;
-  }
-  step += 3;
+  return total;
+}
 
-  console.log(result);
-});
+// Time to check the rest of the slopes - you need to minimize the probability of a sudden arboreal stop, after all.
+
+// Determine the number of trees you would encounter if, for each of the following slopes, you start at the top-left corner and traverse the map all the way to the bottom:
+
+// Right 1, down 1.
+// Right 3, down 1. (This is the slope you already checked.)
+// Right 5, down 1.
+// Right 7, down 1.
+// Right 1, down 2.
+// In the above example, these slopes would find 2, 7, 3, 4, and 2 tree(s) respectively; multiplied together, these produce the answer 336.
+
+console.log(
+  getTrees(1, 1) *
+    getTrees(3, 1) *
+    getTrees(5, 1) *
+    getTrees(7, 1) *
+    getTrees(1, 2)
+);
